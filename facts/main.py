b9760c67fc50e886f6b04a1974442854776e2173
Matteo Vannini <136280542+heisenbrook@users.noreply.github.com>
2026-01-30 17:24:51 +0000

    Embeddings

--- facts/main.py
+++ facts/main.py
@@ -1,5 +1,7 @@
 import os
 from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI  
 from langchain.chains import LLMChain      
 from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
@@ -12,6 +14,25 @@ print('COURSE_KEY present?', os.getenv('COURSE_KEY') is not None)
 
 api_key = os.getenv('COURSE_KEY')
 
-# Load .txt with Langchain
+# Initialize Text-splitter
+text_splitter = CharacterTextSplitter(
+    separator='\n',
+    chunk_size=200,
+    chunk_overlap=0
+)
 
+# Load .txt with Langchain and show the split results
 loader = TextLoader('facts.txt')
+docs = loader.load_and_split()
+
+for doc in docs:
+    print(doc.page_content)
+    print('\n')
+
+# Embeddings
+# ===================
+
+embeddings = OpenAIEmbeddings(
+    api_key=api_key
+)
+
